c6cfead644ebb8f9940992552661405a215e540f
Furkan söker <furkan.soker@gmail.com>
2025-12-20 20:57:43 +0300

    Puls Efect Added

--- src/data/turkeyLocations.js
+++ src/data/turkeyLocations.js
@@ -1,3 +1,11 @@
+const TURKEY_LOCATIONS = [
+  { city: 'Istanbul', districts: ['Kadikoy', 'Besiktas', 'Uskudar'] },
+  { city: 'Ankara', districts: ['Cankaya', 'Kecioren', 'Sincan'] },
+  { city: 'Izmir', districts: ['Konak', 'Bornova', 'Karsiyaka'] },
+  { city: 'Bursa', districts: ['Osmangazi', 'Nilufer', 'Yildirim'] },
+  { city: 'Antalya', districts: ['Muratpasa', 'Konyaalti', 'Kepez'] },
+]
+
 const sortedLocations = [...TURKEY_LOCATIONS].sort((a, b) =>
   a.city.localeCompare(b.city, 'tr')
 )
